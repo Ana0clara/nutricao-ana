3101cf26e79367ea6ddf9430dba9652eb703a290
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2025-11-19 13:12:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var tituloPagina = document.getElementById('titulo-pagina');
+tituloPagina.textContent = "Ana nutrição";
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for(var contador = 0; contador <= 4; contador ++) {
+  var tdPeso = pacientes[contador].querySelectorAll("");
+  }
+
+//var nome = document.getElementById('nome');
+//nome.textContent = "joana";
+
+//var celulaPeso = document.getElementById('peso');
+//var peso = celulaPeso.textContent;
+
+//var celulaAltura = document.getElementById('altura');
+//var altura = celulaAltura.textContent;
+
+//var celulaGordura = document.getElementById('gordura');
+//var gordura = celulaGordura.textContent;
+
+//if (peso < 0) {
+//  celulaPeso.textContent = "peso invalido";}
+
+//if (altura < 0){
+ // celulaAltura.textContent = "altura invalida";}
+
+//if (gordura < 0){
+ // celulaGordura.textContent = "gordura invalida";}
+
