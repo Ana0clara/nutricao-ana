f2558bfd3f4924ad16decf0892224a21e7afef30
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2025-11-19 13:38:09 +0000

    19/11

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ var tituloPagina = document.getElementById('titulo-pagina');
 tituloPagina.textContent = "Ana nutrição";
 
 var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
 
 for(var contador = 0; contador <= 4; contador ++) {
-  var tdPeso = pacientes[contador].querySelectorAll("");
+  
+  var tdPeso = pacientes[contador].querySelector("*.info-peso");
+  var peso = tdPeso.textContent;
+  
+  var tdAltura = pacientes[contador].querySelector("*.info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdGordura = pacientes[contador].querySelector("*.info-gordura");
+  var gordura = tdGordura.textContent
+
+  var tdImc = pacientes[contador].querySelector("*.info-imc");
+  var imc = tdImc.textContent;
+
+  var pesoEhvalido = true;
+  var alturaEhvalida = true;
+
+  if(peso < 0 ) {
+    tdPeso.textContent = "Peso invalido";
+  }
+
+  if(altura < 0){
+    tdAltura.textContent = "altura invalida";
+  }
+    
   }
 
 //var nome = document.getElementById('nome');
