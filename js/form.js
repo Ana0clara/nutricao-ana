ca74d52fbb13604c72af708e8950ce3637e9bb87
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2025-12-03 11:37:25 +0000

    03/12

--- js/form.js
+++ js/form.js
@@ -6,9 +6,13 @@ botaoAdicionar.addEventListener("click",function(event){
   
     //extrair os dados do formulario
      var paciente = obtemPacienteDoFormulario(form);
-
+  
     //criando a linha e as celulas do novo paciente
-    var pacienterTr = montaTr(paciente);
+    var pacienteTr = montaTr(paciente);
+
+    //alocando a linha na tabela
+    var tabela = document.querySelector('#tabela-pacientes');
+    tabela.appendChild(pacienteTr);
   
 });
 
@@ -23,8 +27,25 @@ function obtemPacienteDoFormulario(form) {
   return paciente;
 }
 
-function montaTr(){
-  
+function montaTr(paciente){
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+  return pacienteTr;
+}
+
+function montaTd(dado, classe){
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+
+  return td;
 }
 
 
